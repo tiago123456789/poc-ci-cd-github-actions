5a108a88515cf74ac09f21c753623f1498c1a432
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-25 20:28:34 -0300

    Send poc for project github

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const app = require("express")();
+
+app.get("/", (request, response) => {
+    response.json({
+        message: "Poc CI/CD using Github Actions is working!!!"
+    })
+})
+
+app.listen(
+    process.env.PORT, 
+    () => console.log(`Application is running in address: ${process.env.APP_URL}`)
+);
