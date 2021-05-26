a90195cf4ca9c100ace0434ce2ed404d4638eecc
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-25 23:05:51 -0300

    Changes v3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = require("express")();
 
 app.get("/", (request, response) => {
     response.json({
-        message: "V2 Poc CI/CD using Github Actions is working!!!"
+        message: "V3 Poc CI/CD using Github Actions is working!!!"
     })
 })
 
