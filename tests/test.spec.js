01cf08f25fd12e082e30c561b007f07f706c6269
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-25 22:57:39 -0300

    Adding test

---
+++ tests/test.spec.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('TDD de Operações da Calculadora', () => {
+    it('Teste: Deve Somar 2 Números', () => {
+      assert.equal(true, true);
+    });
+  });
