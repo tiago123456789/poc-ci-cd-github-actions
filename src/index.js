662f367f4bdd3b3f463648d834a6c6ea01ec4042
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-29 17:17:57 -0300

    Change v15 and remove add secret for id and token discord webhook

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = require("express")();
 
 app.get("/", (request, response) => {
     response.json({
-        message: "V14 Poc CI/CD using Github Actions is working!!!"
+        message: "V15 Poc CI/CD using Github Actions is working!!!"
     })
 })
 
