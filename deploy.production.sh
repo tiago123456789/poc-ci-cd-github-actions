387b544cfa772731f56d3d7cecf0a9ee1e4923dd
Tiago rosa da costa <tiagorosadacost@gmail.com>
2021-05-29 16:42:24 -0300

    Change v11 and change script deploy production

--- deploy.production.sh
+++ deploy.production.sh
@@ -3,6 +3,7 @@
 cd app
 npm install --only=production
 git fetch
+echo $(git describe --tags) > ./previous-tag.txt
 git checkout $(git for-each-ref refs/tags --sort=-taggerdate --format='%(refname)' --count=1)
 ./node_modules/.bin/pm2 restart all
 ./node_modules/.bin/pm2 save
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = require("express")();
 
 app.get("/", (request, response) => {
     response.json({
-        message: "V10 Poc CI/CD using Github Actions is working!!!"
+        message: "V11 Poc CI/CD using Github Actions is working!!!"
     })
 })
 
